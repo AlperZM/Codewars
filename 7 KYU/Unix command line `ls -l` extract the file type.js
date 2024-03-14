f64b19a704d7219804f1449df1243ffa9a29a234
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-03-14 10:54:05 +0300

    Create Unix command line `ls -l` extract the file type.js

---
+++ 7 KYU/Unix command line `ls -l` extract the file type.js
@@ -0,0 +1,14 @@
+function linuxType(fileAttribute) {
+let types = {
+    '-': 'file',
+    'd': 'directory',
+    'l': 'symlink',
+    'c': 'character_file',
+    'b': 'block_file',
+    'p': 'pipe',
+    's': 'socket',
+    'D': 'door',
+  };
+  
+  return types[fileAttribute[0]];
+}
