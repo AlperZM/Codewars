be6e35f3f91367e4d4c972df806f070eddf57b49
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-03-15 10:50:40 +0300

    Update Histogram - H1.js

--- 7 KYU/Histogram - H1.js
+++ 7 KYU/Histogram - H1.js
@@ -1,3 +1,8 @@
 function histogram(results) {
-  return `6|${"#".repeat(results[5])}${results[5] > 0 ? " " + results[5] : ""}\n5|${"#".repeat(results[4])}${results[4] > 0 ? " " + results[4] : ""}\n4|${"#".repeat(results[3])}${results[3] > 0 ? " " + results[3] : ""}\n3|${"#".repeat(results[2])}${results[2] > 0 ? " " + results[2] : ""}\n2|${"#".repeat(results[1])}${results[1] > 0 ? " " + results[1] : ""}\n1|${"#".repeat(results[0])}${results[0] > 0 ? " " + results[0] : ""}\n`;
+  var histogram = '';
+  
+  for (var i=5; i>=0; --i)
+    histogram += (i+1) + '|' + '#'.repeat(results[i]) + (results[i] > 0 ? ' ' + results[i] : '') + "\n";
+  
+  return histogram;
 }
