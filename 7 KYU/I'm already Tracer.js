173fcdc6f6aed4bf8ac2b8f0b7850ad39551bb69
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-03-25 09:37:44 +0300

    Create I'm already Tracer.js

---
+++ 7 KYU/I'm already Tracer.js
@@ -0,0 +1,6 @@
+function teamComp(heroes) {
+  if (heroes.length != 6 || new Set(heroes).size != heroes.length) throw "GG";
+  return [TANK, DAMAGE, SUPPORT].map(
+    arr => arr.filter(h => heroes.indexOf(h) >= 0).length
+  );
+}
