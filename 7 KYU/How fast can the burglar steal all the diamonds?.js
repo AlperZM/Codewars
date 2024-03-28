cdc074faa07f593c7210d1589340980140a62954
AlperZM <35838758+AlperZM@users.noreply.github.com>
2024-03-28 12:16:33 +0300

    Update How fast can the burglar steal all the diamonds?.js

--- 7 KYU/How fast can the burglar steal all the diamonds?.js
+++ 7 KYU/How fast can the burglar steal all the diamonds?.js
@@ -4,3 +4,8 @@ function diamondBurglar(locker) {
     .map(m => m.length <= 2 ? res += 1 : m.length % 2 === 0 ? res += m.length / 2 : res += Math.ceil(m.length / 2))
   return res
 }
+// is equals to
+
+function diamondBurglar(locker) {
+  return locker.join(".").match(/[*]{1,2}/g || []).length
+}
